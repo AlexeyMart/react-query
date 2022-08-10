a05d6fcac79a82cfa7b235dbd6b7544b9ddf2fc3
Alex Mart <alexey.martyshev@mail.ru>
2022-08-10 23:00:34 +0300

    default RQ params

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,23 @@
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import App from "./App";
 import { staleTime } from "./constants";
+import "./index.css";
 
+// options также можно применять в каждом отдельном useQuery
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       retry: 0, // default = 3
       staleTime, // default = 0
-      // defaults
-      // cacheTime: 300000,
+      // defaults params
+      cacheTime: 300000, // 5 minutes
+      refetchOnMount: true, // refetch on mount if data is stale. values: true | false | 'always'
+      refetchOnWindowFocus: true, // refetch on window focus after losing it if data is stale. true | false | 'always'
+      refetchInterval: false, // polling data. false | number (ms)
+      refetchIntervalInBackground: false, // при потере фокуса окна refetchInterval встает на паузу.
     },
   },
 });
