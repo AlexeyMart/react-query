a42794a87c78f70d67d078392736381e5aa540a2
Alex Mart <alexey.martyshev@mail.ru>
2022-08-12 18:27:28 +0300

    дополнительная типизация useQueries

--- src/queries/useParallelSuperHeroesData.ts
+++ src/queries/useParallelSuperHeroesData.ts
@@ -1,9 +1,10 @@
-import { useQueries } from "@tanstack/react-query";
+import { QueryOptions, useQueries } from "@tanstack/react-query";
 import { fetchSuperHero } from "./useSuperHeroData";
 import { QueryKeys } from "../constants";
+import { SuperHero } from "../types";
 
 export const useParallelSuperHeroesData = (heroIds: string[]) => {
-  const queries = heroIds.map((heroId) => ({
+  const queries: QueryOptions<SuperHero>[] = heroIds.map((heroId) => ({
     queryKey: [QueryKeys.SuperHero, heroId],
     queryFn: () => fetchSuperHero(heroId),
   }));
