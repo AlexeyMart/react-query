6f40ada97e8758672651f8c2d1c72478979cf547
Alex Mart <alexey.martyshev@mail.ru>
2022-11-02 18:22:25 +0500

    STI

--- src/components/pages/HomePage/HomePage.tsx
+++ src/components/pages/HomePage/HomePage.tsx
@@ -1,6 +1,36 @@
-import { FC } from "react";
-import { Typography } from "antd";
+import { FC, useState, useEffect, useRef } from "react";
+import { Typography, Button } from "antd";
 
 export const HomePage: FC = () => {
-  return <Typography.Title>Home Page</Typography.Title>;
+  const [time, setTime] = useState(0);
+
+  const refInterval = useRef<null | number>(null);
+
+  useEffect(() => {
+    if (time > 0 && !refInterval.current) {
+      refInterval.current = window.setInterval(() => {
+        setTime((time) => time - 1);
+      }, 1000);
+    }
+
+    if (time === 0 && refInterval.current) {
+      window.clearInterval(refInterval.current);
+
+      refInterval.current = null;
+    }
+  }, [time]);
+
+  const handleClick = () => {
+    setTime(15);
+  };
+
+  return (
+    <>
+      <Typography.Title>Home Page {time}</Typography.Title>
+
+      <Button disabled={time !== 0} onClick={handleClick}>
+        Refresh
+      </Button>
+    </>
+  );
 };
